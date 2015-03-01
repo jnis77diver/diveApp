7cacb100448972d764b1036a34eaaa7e9c98f76a
Jonah Nisenson <jsnisenson@gmail.com>
2015-02-28 23:53:53 -0500

    diveApp posts and gets messages stored in Firebase database

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,96 @@ var express = require('express');
 var router = express.Router();
 var appdata = require('../data1.json');
 var jf           = require('jsonfile');
-var file    = 'data1.json'
+var file    = 'data1.json';
+var Firebase = require('firebase');
 // ROUTES FOR OUR API
 // =============================================================================
+var ref = new Firebase('https://diveapp.firebaseio.com/');
+var divingRef = ref.child("diving");
+var snorkelingRef = ref.child("snorkeling");
+
+var allExperiences = [];
+var diveExperiences = [];
+var snorkelExperiences = [];
+var dive = [];
+var snorkel = [];
+var objData;
+var divingData;
+var snorkelingData;
 
 router.use(function(req, res, next) {
 	// do logging
 	console.log('Wish Engine is working.');
 	next(); // make sure we go to the next routes and don't stop here
 });
-/* GET home page. */
+/* GET homepage. */
 router.get('/', function(req, res) {
-	var allExperiences = [],
-	    diveExperiences = [],
-	    snorkelExperiences = [],
-	    dive = [],
-	    snorkel = [];
+  ref.on("value", function(snapshot) {
+    console.log("snapshot", snapshot.val());
+    objData = snapshot.val();
+    console.log("objData is ", objData);
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+  divingRef.on("value", function(snapshot) {
+    console.log("snapshot", snapshot.val());
+    divingData = snapshot.val();
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+  snorkelingRef.on("value", function(snapshot) {
+    console.log("snapshot", snapshot.val());
+    snorkelingData = snapshot.val();
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+
+
+
+
 	//diveExperiences = appdata.diving;
 	//snorkelExperiences = appdata.snorkeling;
+  for( var key in objData ) {
+   for ( var key2 in objData[key] ) {
+      allExperiences.push(objData[key][key2]);
+   }
+  }
+  for( var key in divingData ) {
+    for ( var key2 in divingData[key] ) {
+      diveExperiences.push(divingData[key][key2]);
+    }
+  }
+  for( var key in snorkelingData ) {
+    for ( var key2 in snorkelingData[key] ) {
+      snorkelExperiences.push(snorkelingData[key][key2]);
+    }
+  }
+  //objData.diving.forEach(function(item) {
+  //  allExperiences.push(item);
+  //});
+  //objData.snorkeling.forEach(function(item) {
+  //  allExperiences.push(item);
+  //});
+  allExperiences.sort(function(a, b) {
+    if(new Date(a.date) < new Date(b.date)) { return 1;	}
+    if(new Date(a.date) > new Date(b.date)) { return -1; }
+    return 0;
+  });
 
-	appdata.diving.forEach(function(item) {
-		allExperiences.push(item);
-	});
-	appdata.snorkeling.forEach(function(item) {
-		allExperiences.push(item);
-	});
-	allExperiences.sort(function(a, b) {
-		if(new Date(a.date) < new Date(b.date)) { return 1;	}
-		if(new Date(a.date) > new Date(b.date)) { return -1; }
-		return 0;
-	}); 
+
+
+  //appdata.diving.forEach(function(item) {
+	//	allExperiences.push(item);
+	//});
+	//appdata.snorkeling.forEach(function(item) {
+	//	allExperiences.push(item);
+	//});
+	//allExperiences.sort(function(a, b) {
+	//	if(new Date(a.date) < new Date(b.date)) { return 1;	}
+	//	if(new Date(a.date) > new Date(b.date)) { return -1; }
+	//	return 0;
+	//});
 
   res.render('index', { 
   	title: 'Home',
@@ -39,8 +99,9 @@ router.get('/', function(req, res) {
   	diveExperiences: diveExperiences,
   	snorkelExperiences: snorkelExperiences,
   	page: 'home'
-  	 });
+  });
 });
+
 
 // create a dive/snorkel post
 router.route('/dives')
@@ -49,12 +110,15 @@ router.route('/dives')
 		 jf.readFile(file, function(err, obj) {
 		 	console.log('A post request was made.');
 		 	if(req.body.type === 'diving') {
-		 		var dive_id = obj.diving.push(req.body);
+		 		//var dive_id = obj.diving.push(req.body);
+        divingRef.push(req.body);
 		 	} else if (req.body.type === 'snorkeling') {
-		 		var snorkel_id = obj.snorkeling.push(req.body);
-		 	}
+		 		//var snorkel_id = obj.snorkeling.push(req.body);
+        snorkelingRef.push(req.body);
 
-		    
+      }
+
+
 		    jf.writeFile(file, obj, function(err) {
 			   if (err) {
 			 		res.send(err);
@@ -67,13 +131,21 @@ router.route('/dives')
 	// retrieve all posts
 
 .get(function(req, res) {
-		jf.readFile(file, function(err, obj) {
-		  if (err) {
-				res.send(err);
-		  } else  {
-		  	res.json(obj.snorkeling);
-		  }			
-		})
+    ref.on("value", function(snapshot) {
+      snapshot.val();
+      console.log(snapshot.val());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
+
+		//jf.readFile(file, function(err, obj) {
+		//  if (err) {
+		//		res.send(err);
+		//  } else  {
+		//  	res.json(obj.snorkeling);
+		//  }
+		//})
 	});
 
 
@@ -130,3 +202,6 @@ router.get('/artists/:artistid', function(req, res) {
 
 
 module.exports = router;
+
+
+//take that
